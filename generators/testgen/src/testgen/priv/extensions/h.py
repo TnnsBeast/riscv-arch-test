e2ed9c393701871fae913a8ab0fca527c7a14002
TnnsBeast <neil22@me.com>
2026-02-24 22:01:19 -0800

    fix typo

--- generators/testgen/src/testgen/priv/extensions/h.py
+++ generators/testgen/src/testgen/priv/extensions/h.py
@@ -234,7 +234,7 @@ def _generate_hcsr_rv32_access_tests(test_data: TestData) -> list[str]:
 
     # High-half CSRs only exist when XLEN=32.
     csrs = [
-        "hedelegh"
+        "hedelegh",
         "henvcfgh",
         "htimedeltah",
         "vstimecmph",
